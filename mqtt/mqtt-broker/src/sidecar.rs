aa9d57376acc61807572122eb15f5249a6525d75
Denis Molokanov <dmolokanov@users.noreply.github.com>
2020-10-30 00:02:56 -0700

    add comments to sidecar

--- mqtt/mqtt-broker/src/sidecar.rs
+++ mqtt/mqtt-broker/src/sidecar.rs
@@ -1,15 +1,20 @@
 use std::{future::Future, pin::Pin};
 
+use async_trait::async_trait;
 use futures_util::FutureExt;
 use tokio::sync::mpsc::{self, Receiver, Sender};
 
-#[async_trait::async_trait]
+/// A common trait for any additional routine that enriches MQTT broker behavior.
+#[async_trait]
 pub trait Sidecar {
+    /// Returns a new instance of a shutdown handle to be used to stop sidecar.
     fn shutdown_handle(&self) -> Result<SidecarShutdownHandle, SidecarShutdownHandleError>;
 
+    /// Starts a routine.
     async fn run(self: Box<Self>);
 }
 
+/// Shutdown handle to request a sidecar to stop.
 pub struct SidecarShutdownHandle(Pin<Box<dyn Future<Output = ()>>>);
 
 impl SidecarShutdownHandle {
@@ -25,18 +30,21 @@ impl SidecarShutdownHandle {
     }
 }
 
+/// This error returned when there is impossible to obtain a shutdown handle.
 #[derive(Debug, thiserror::Error)]
 #[error("unable to obtain shutdown handler for sidecar")]
 pub struct SidecarShutdownHandleError;
 
+/// Creates a new instance of `PendingSidecar`.
 pub fn pending() -> PendingSidecar {
     let (rx, tx) = mpsc::channel(1);
     PendingSidecar(rx, tx)
 }
 
+/// A stub sidecar which does not do any work and just waits for shutdown signal.
 pub struct PendingSidecar(Sender<()>, Receiver<()>);
 
-#[async_trait::async_trait]
+#[async_trait]
 impl Sidecar for PendingSidecar {
     fn shutdown_handle(&self) -> Result<SidecarShutdownHandle, SidecarShutdownHandleError> {
         let mut handle = self.0.clone();
